690e0600270efca1545612cf82ebe07fe6732a6b
Timur <tima.temirov.2005@mail.ru>
2024-03-11 18:02:24 +0300

    added buttons and buttons' logic

--- secondwindow.cpp
+++ secondwindow.cpp
@@ -1,14 +1,18 @@
 #include "secondwindow.h"
 #include "ui_secondwindow.h"
 
-SecondWindow::SecondWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::SecondWindow)
-{
+SecondWindow::SecondWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::SecondWindow) {
     ui->setupUi(this);
+    third_window = new ThirdWindow();
 }
 
-SecondWindow::~SecondWindow()
-{
+SecondWindow::~SecondWindow() {
     delete ui;
+    delete third_window;
 }
+
+void SecondWindow::on_pushButtonNext_clicked() {
+    this->close();
+    third_window->show();
+}
+
--- secondwindow.h
+++ secondwindow.h
@@ -2,6 +2,7 @@
 #define SECONDWINDOW_H
 
 #include <QMainWindow>
+#include <thirdwindow.h>
 
 namespace Ui {
 class SecondWindow;
@@ -15,8 +16,12 @@ public:
     explicit SecondWindow(QWidget *parent = nullptr);
     ~SecondWindow();
 
+private slots:
+    void on_pushButtonNext_clicked();
+
 private:
     Ui::SecondWindow *ui;
+    ThirdWindow* third_window;
 };
 
 #endif // SECONDWINDOW_H
